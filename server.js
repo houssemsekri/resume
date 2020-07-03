60749a3298d5767baa62e03278cc2604a85fd6d1
houssemsekri64 <61097885+houssemsekri64@users.noreply.github.com>
2020-07-03 23:04:46 +0100

    prepare

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
-
+const path = require("path");
 const connect = require("./config/db");
 const cors = require("cors");
+const env = require("dotenv").config();
 const app = express();
 app.use(
   cors({
@@ -11,7 +12,15 @@ app.use(
 connect();
 app.use(express.json());
 app.use("/api", require("./api/houssem"));
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
 
-app.listen(5000, () => {
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
   console.log("connected");
 });
